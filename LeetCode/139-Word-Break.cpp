00825909facab323a928b32f5ab3570362559b59
architjain11 <architjain@gmail.com>
2022-12-16 00:17:55 +0530

    new ques solved

---
+++ LeetCode/139-Word-Break.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n=s.size();
+        vector<bool> dp(n+1, false);
+        dp[n]=true;
+        for(int i=n-1; i>=0; i--) {
+            for(auto word: wordDict) {
+                if(i+word.size()<=s.size() && s.substr(i, word.size())==word)
+                    dp[i]=dp[i+word.size()];
+                if(dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+};
