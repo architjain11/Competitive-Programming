55cf41b24a57b7b200d17f233533d0cb402188aa
architjain11 <architjain@gmail.com>
2023-03-21 23:39:43 +0530

    daily challenge

---
+++ LeetCode/2348-Number-of-Zero-Filled-Subarrays.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans=0;
+        for(int i=0, j=0; i<nums.size(); i++) {
+            if(nums[i]!=0) j=i+1;
+            ans+=i-j+1;
+        }
+        return ans;
+    }
+};
