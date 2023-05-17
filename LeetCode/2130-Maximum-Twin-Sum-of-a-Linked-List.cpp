fd9e2578f65241f8001d54fcc592fe7701b78461
architjain11 <architjain@gmail.com>
2023-05-17 15:03:05 +0530

    daily challenge

---
+++ LeetCode/2130-Maximum-Twin-Sum-of-a-Linked-List.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        vector<int> vals;
+        while(head!=NULL) {
+            vals.push_back(head->val);
+            head=head->next;
+        }
+
+        int ans=0, n=vals.size();
+        for(int i=0; i<n/2; ++i)
+            ans=max(ans, vals[i]+vals[n-i-1]);
+        
+        return ans;
+    }
+};
