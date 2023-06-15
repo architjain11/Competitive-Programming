f87f37d38e489aa191483494d3495d613cfe3375
architjain11 <architjain@gmail.com>
2023-06-15 16:00:02 +0530

    daily challenge

---
+++ LeetCode/1161-Maximum-Level-Sum-of-a-Binary-Tree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int maxSum=INT_MIN;
+        int level=1, ans=1;
+
+        while(!q.empty()) {
+            int n=q.size();
+            int sum=0;
+            while(n--) {
+                TreeNode* node=q.front();
+                q.pop();
+                sum+=node->val;
+                if(node->right) q.push(node->right);
+                if(node->left) q.push(node->left);
+            }
+            if(maxSum<sum) {
+                maxSum=sum;
+                ans=level;
+            }
+            level++;
+        }
+        return ans;
+    }
+};
