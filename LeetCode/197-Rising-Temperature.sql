959156e62bb4b62f9cd31c4b835bab0b27625724
architjain11 <architjain@gmail.com>
2023-07-18 02:22:38 +0530

    new sql ques

---
+++ LeetCode/197-Rising-Temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select weather.id as id
+from weather
+join weather w
+on datediff(weather.recordDate, w.recordDate)=1
+and weather.temperature>w.temperature;
---
+++ LeetCode/577-Employee-Bonus.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select name, bonus
+from employee
+left join bonus
+on employee.empId=bonus.empId
+where bonus<1000 or bonus is null;
