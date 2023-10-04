f6c2db8c7d488968ba3517776e66d7d6e8d410a7
Archit Jain <architjain@gmail.com>
2023-10-04 21:49:54 +0530

    daily challenge

---
+++ LeetCode/706-Design-HashMap.cpp
@@ -0,0 +1,28 @@
+class MyHashMap {
+public:
+    vector<int> m;
+
+    MyHashMap() {
+        m.resize(1000001, -1);
+    }
+    
+    void put(int key, int value) {
+        m[key]=value;
+    }
+    
+    int get(int key) {
+        return m[key];
+    }
+    
+    void remove(int key) {
+        m[key]=-1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
