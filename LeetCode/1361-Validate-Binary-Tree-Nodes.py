17ae9f2c75e6a67e1bb38476355aae33229f1940
Archit Jain <architjain@gmail.com>
2023-10-17 23:14:18 +0530

    daily challenge

---
+++ LeetCode/1361-Validate-Binary-Tree-Nodes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        hasParent=set(leftChild+rightChild)
+        hasParent.discard(-1)
+        if n==len(hasParent): return False
+
+        root=-1
+        for i in range(n):
+            if i not in hasParent:
+                root=i
+                break
+
+        visited=set()
+        def dfs(i):
+            if i==-1: return True
+            if i in visited: return False
+
+            visited.add(i)
+            return dfs(leftChild[i]) and dfs(rightChild[i])
+
+        return dfs(root) and len(visited)==n
