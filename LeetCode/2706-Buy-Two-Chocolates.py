c170d0239f7d1bc9cbeae7d95bfc4d57339ae481
Archit Jain <architjain@gmail.com>
2023-12-20 22:34:46 +0530

    daily challenge

---
+++ LeetCode/2706-Buy-Two-Chocolates.py
@@ -0,0 +1,6 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+        prices.sort()
+        cost = prices[0]+prices[1]
+
+        return money-cost if cost<=money else money
