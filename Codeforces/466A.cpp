a573f38cfeebcd9c6ef07166fc9e4f0cdf7383e9
Archit Jain <architjain@gmail.com>
2024-01-01 23:51:03 +0530

    new cf ques, modify gitignore to exclude cph

---
+++ Codeforces/466A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m, a, b;
+    cin>>n>>m>>a>>b;
+
+    if(m*a>b)
+        cout<<((n/m)*b)+min((n%m)*a, b);
+    else
+        cout<<n*a;
+
+    return 0;
+}
