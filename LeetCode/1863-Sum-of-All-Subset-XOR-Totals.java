96879ed399d0151e1cd791cab70c68b48e67d7ce
Archit Jain <architjain@gmail.com>
2024-05-21 00:42:43 +0530

    daily challenge

---
+++ LeetCode/1863-Sum-of-All-Subset-XOR-Totals.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int ans=0;
+        for(int num: nums)
+            ans |= num;
+
+        return ans<<(nums.length-1);
+    }
+}
